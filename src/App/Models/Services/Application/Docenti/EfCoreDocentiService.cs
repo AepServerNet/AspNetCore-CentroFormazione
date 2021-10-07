430fd4a3757c59684ded78a20b701905177c7faf
Angelo Pirola <angelo9871@gmail.com>
2021-10-07 13:34:13 +0200

    Aggiornato servizio applicativo Docenti

--- src/App/Models/Services/Application/Docenti/EfCoreDocentiService.cs
+++ src/App/Models/Services/Application/Docenti/EfCoreDocentiService.cs
@@ -7,6 +7,9 @@ using App.Models.ViewModels.Docenti;
 using App.Models.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using App.Models.ViewModels;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace App.Models.Services.Application.Docenti
 {
@@ -19,6 +22,40 @@ namespace App.Models.Services.Application.Docenti
         {
             this.logger = logger;
             this.dbContext = dbContext;
+        }
+
+        public async Task<ListViewModel<DocenteViewModel>> GetDocentiAsync(DocenteListInputModel model)
+        {
+            IQueryable<Docente> baseQuery = dbContext.Docenti;
+
+            baseQuery = (model.OrderBy, model.Ascending) switch
+            {
+                ("NominativoDocente", true) => baseQuery.OrderBy(docente => docente.NominativoDocente),
+                ("NominativoDocente", false) => baseQuery.OrderByDescending(docente => docente.NominativoDocente),
+                ("Id", true) => baseQuery.OrderBy(docente => docente.Id),
+                ("Id", false) => baseQuery.OrderByDescending(docente => docente.Id),
+                _ => baseQuery
+            };
+
+            IQueryable<Docente> queryLinq = baseQuery
+                .Where(docente => docente.NominativoDocente.Contains(model.Search))
+                .AsNoTracking();
+
+            List<DocenteViewModel> docente = await queryLinq
+                .Skip(model.Offset)
+                .Take(model.Limit)
+                .Select(docente => docente.ToDocenteViewModel())
+                .ToListAsync();
+
+            int totalCount = await queryLinq.CountAsync();
+
+            ListViewModel<DocenteViewModel> result = new()
+            {
+                Results = docente,
+                TotalCount = totalCount
+            };
+
+            return result;
         }
 
         public async Task<DocenteDetailViewModel> CreateDocenteAsync(DocenteCreateInputModel inputModel)
@@ -39,6 +76,82 @@ namespace App.Models.Services.Application.Docenti
             }
 
             return docente.ToDocenteDetailViewModel();
+        }
+
+        public async Task<DocenteEditInputModel> GetDocenteForEditingAsync(string IdDocente)
+        {
+            IQueryable<DocenteEditInputModel> queryLinq = dbContext.Docenti
+                .AsNoTracking()
+                .Where(docente => docente.IdDocente == IdDocente)
+                .Select(docente => docente.ToDocenteEditInputModel());
+
+            DocenteEditInputModel viewModel = await queryLinq.FirstOrDefaultAsync();
+
+            if (viewModel == null)
+            {
+                // TODO
+                logger.LogWarning("Docente {id} non trovato", IdDocente);
+                //throw new DocenteNotFoundException(IdDocente);
+            }
+
+            return viewModel;
+        }
+
+        public async Task<DocenteDetailViewModel> EditDocenteAsync(DocenteEditInputModel inputModel)
+        {
+            Docente docente = await dbContext.Docenti.FindAsync(inputModel.Id);
+
+            if (docente == null)
+            {
+                // TODO
+                logger.LogWarning("Docente {id} non trovato", inputModel.IdDocente);
+                //throw new DocenteNotFoundException(inputModel.IdDocente);
+            }
+
+            docente.ChangeNominativoDocente(inputModel.NominativoDocente);
+            docente.ChangeMateriaInsegnata(inputModel.MateriaInsegnata);
+            docente.ChangeTelefono(inputModel.Telefono);
+            docente.ChangeEmail(inputModel.Email);
+            docente.ChangeResidenza(inputModel.Residenza);
+            docente.ChangeCodiceCorso(inputModel.CodiceCorso);
+            docente.ChangeCodiceDipartimento(inputModel.CodiceDipartimento);
+            docente.ChangeCostoOrario(inputModel.CostoOrario);
+
+            await dbContext.SaveChangesAsync();
+            return docente.ToDocenteDetailViewModel();
+        }
+
+        public async Task<DocenteDetailViewModel> GetDocenteAsync(string IdDocente)
+        {
+            IQueryable<DocenteDetailViewModel> queryLinq = dbContext.Docenti
+                .AsNoTracking()
+                .Where(docente => docente.IdDocente == IdDocente)
+                .Select(docente => docente.ToDocenteDetailViewModel());
+
+            DocenteDetailViewModel viewModel = await queryLinq.FirstOrDefaultAsync();
+
+            if (viewModel == null)
+            {
+                // TODO
+                logger.LogWarning("Docente {IdDocente} non trovato", IdDocente);
+                //throw new DocenteNotFoundException(IdDocente);
+            }
+
+            return viewModel;
+        }
+
+        public async Task DeleteDocenteAsync(DocenteDeleteInputModel inputModel)
+        {
+            Docente docente = await dbContext.Docenti.FindAsync(inputModel.IdDocente);
+
+            if (docente == null)
+            {
+                // TODO
+                //throw new DocenteNotFoundException(inputModel.IdDocente);
+            }
+
+            dbContext.Remove(docente);
+            await dbContext.SaveChangesAsync();
         }
     }
 }
