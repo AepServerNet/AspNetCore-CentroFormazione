80501279060b7e785058bb8b010e3cd33f0e1f5a
Angelo Pirola <angelo9871@gmail.com>
2021-10-07 13:31:18 +0200

    Aggiunte classi static extension method Docenti

--- src/App/Models/Extensions/DocentiExtension.cs
+++ src/App/Models/Extensions/DocentiExtension.cs
@@ -1,13 +1,48 @@
 using App.Models.Entities;
+using App.Models.InputModels.Docenti;
 using App.Models.ViewModels.Docenti;
 
 namespace App.Models.Extensions
 {
     public static class DocentiExtension
     {
+        public static DocenteViewModel ToDocenteViewModel(this Docente docente)
+        {
+            return new DocenteViewModel
+            {
+                Id = docente.Id,
+                IdDocente = docente.IdDocente,
+                NominativoDocente  = docente.NominativoDocente,
+                MateriaInsegnata = docente.MateriaInsegnata,
+                Telefono = docente.Telefono,
+                Email = docente.Email,
+                Residenza = docente.Residenza,
+                CodiceCorso = docente.CodiceCorso,
+                CodiceDipartimento = docente.CodiceDipartimento,
+                CostoOrario = docente.CostoOrario
+            };
+        }
+
         public static DocenteDetailViewModel ToDocenteDetailViewModel(this Docente docente)
         {
             return new DocenteDetailViewModel 
+            {
+                Id = docente.Id,
+                IdDocente = docente.IdDocente,
+                NominativoDocente  = docente.NominativoDocente,
+                MateriaInsegnata = docente.MateriaInsegnata,
+                Telefono = docente.Telefono,
+                Email = docente.Email,
+                Residenza = docente.Residenza,
+                CodiceCorso = docente.CodiceCorso,
+                CodiceDipartimento = docente.CodiceDipartimento,
+                CostoOrario = docente.CostoOrario
+            };
+        }
+
+        public static DocenteEditInputModel ToDocenteEditInputModel(this Docente docente)
+        {
+            return new DocenteEditInputModel
             {
                 Id = docente.Id,
                 IdDocente = docente.IdDocente,
