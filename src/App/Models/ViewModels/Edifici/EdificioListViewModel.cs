33dafd4e0cedb4f5e0fe146762c55a87b761d4cc
Angelo Pirola <angelo9871@gmail.com>
2021-10-29 22:29:39 +0200

    Aggiunte ViewModels Edifici

---
+++ src/App/Models/ViewModels/Edifici/EdificioListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using App.Models.InputModels.Edifici;
+using App.Models.ViewModels.Edifici;
+
+namespace App.Models.ViewModels.Edifici
+{
+    public class EdificioListViewModel : IPaginationInfo
+    {
+        public ListViewModel<EdificioViewModel> Edificio { get; set; }
+        public EdificioListInputModel Input { get; set; }
+
+        int IPaginationInfo.CurrentPage => Input.Page;
+        int IPaginationInfo.TotalResults => Edificio.TotalCount;
+        int IPaginationInfo.ResultsPerPage => Input.Limit;
+
+        string IPaginationInfo.Search => Input.Search;
+        string IPaginationInfo.OrderBy => Input.OrderBy;
+
+        bool IPaginationInfo.Ascending => Input.Ascending;
+    }
+}
---
+++ src/App/Models/ViewModels/Edifici/EdificioViewModel.cs
@@ -0,0 +1,16 @@
+namespace App.Models.ViewModels.Edifici
+{
+    public class EdificioViewModel
+    {
+        public int Id { get; set; }
+        public string IdEdificio { get; set; }
+        public string CodiceDipartimento { get; set; }
+        public string Aula { get; set; }
+        public string Indirizzo { get; set; }
+        public string Piano { get; set; }
+        public string Mq { get; set; }
+        public string Laboratorio { get; set; }
+        public string Posti { get; set; }
+        public string Note { get; set; }
+    }
+}
