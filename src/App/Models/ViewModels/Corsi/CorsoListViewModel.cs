3c38927b8d695d077ae9ab795aaefdceecb2f978
Angelo Pirola <angelo9871@gmail.com>
2021-11-01 11:54:35 +0100

    Aggiunte ViewModels Corsi

---
+++ src/App/Models/ViewModels/Corsi/CorsoListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using App.Models.InputModels.Corsi;
+
+namespace App.Models.ViewModels.Corsi
+{
+    public class CorsoListViewModel : IPaginationInfo
+    {
+        public ListViewModel<CorsoViewModel> Corso { get; set; }
+        public CorsoListInputModel Input { get; set; }
+
+        int IPaginationInfo.CurrentPage => Input.Page;
+        int IPaginationInfo.TotalResults => Corso.TotalCount;
+        int IPaginationInfo.ResultsPerPage => Input.Limit;
+
+        string IPaginationInfo.Search => Input.Search;
+        string IPaginationInfo.OrderBy => Input.OrderBy;
+
+        bool IPaginationInfo.Ascending => Input.Ascending;
+    }
+}
